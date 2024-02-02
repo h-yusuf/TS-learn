edfcb3b569d03acbd4ab99f38d7fac5a78904e61
yusuf <hafidhy288@gmail.com>
2024-02-02 21:43:37 +0700

    add file

---
+++ test.js
@@ -0,0 +1,16 @@
+
+// const first = () => {
+//     usestate:
+//     usestate:
+//     usestate:
+//     usestate:
+//     usestate:
+// }
+
+let nama = 'ysuuf'
+let umur = 12+'tahun'
+let experience = 12+'tahun'+2+'bulan'
+
+let sach = [nama,umur,experience]
+
+console.log({sach});
