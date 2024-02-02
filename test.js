9d32bac97d808f18a27bfc7c3f906582138026f2
yusuf <hafidhy288@gmail.com>
2024-02-02 21:52:37 +0700

    woii

--- test.js
+++ test.js
@@ -5,3 +5,15 @@ let experience = 12+'tahun'+2+'bulan'
 let sach = [nama,umur,experience]
 
 console.log({sach});
+asdajfdlaskjfksdfhOJFE qjdoqdojqwhdiuwqnyx EW BiquadFilterNodeQDF
+asdajfdlaskjfksdfhOJFEASD
+asdajfdlaskjfksdfhOJFEASDDAS
+DOMExceptionAS
+DataTransferD
+addEventListenerAS
+DataTransferDAD
+AbortControllerDA
+DataTransferDADAS
+DataTransferDASD
+AD
+asdajfdlaskjfksdfhOJFEAS
