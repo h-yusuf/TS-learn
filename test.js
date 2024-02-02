c1b97c2b50e76013a29a30e71bf874dcbe6be250
yusuf <hafidhy288@gmail.com>
2024-02-02 21:51:31 +0700

    add

--- test.js
+++ test.js
@@ -1,12 +1,3 @@
-
-// const first = () => {
-//     usestate:
-//     usestate:
-//     usestate:
-//     usestate:
-//     usestate:
-// }
-
 let nama = 'ysuuf'
 let umur = 12+'tahun'
 let experience = 12+'tahun'+2+'bulan'
